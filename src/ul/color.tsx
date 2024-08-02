0edac8265a713638fed60a1d838e7846d9f84b94
LiPoWeRaD <32852476+LiPoWeRaD@users.noreply.github.com>
2024-08-02 03:28:49 +0300

    98989
    778

--- src/ul/color.tsx
+++
@@ -1,8 +0,0 @@
-const color = {
-    focus: "fill-hoverCustom dark:fill-hoverCustom",
-    value: "fill-grayCustom dark:fill-blackValueCustom",
-    error: "fill-redCustom",
-    placeholder: "fill-placeholderCustom dark:fill-blackPlaceholderCustom",
-  }
-
-export default color
